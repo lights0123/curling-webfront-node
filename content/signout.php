2dc9087a4290d858e94d6ffcc86f45b456ab6ade
lights0123 <me@lights0123.com>
2016-04-23 16:56:59 -0400

    Remove unnecessary PHP files

--- content/signout.php
+++
@@ -1,3 +0,0 @@
-<?php
-unset($_SESSION['auth'],$_SESSION['uid'],$_SESSION['un'],$_SESSION['email']);
-gotopage("/");
--- content/team.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: Ben
- * Date: 12/21/2015
- * Time: 9:10 PM
- */
