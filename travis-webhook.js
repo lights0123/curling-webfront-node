d967c0fec5f5c2289947b72327367998b1d1cd20
lights0123 <me@lights0123.com>
2017-01-16 13:57:45 -0500

    Fix travis webhooks

--- travis-webhook.js
+++ travis-webhook.js
@@ -1,7 +1,7 @@
 /* Note: this mostly comes from https://github.com/chrisjaure/travisci-webhook-handler
  * modified to support express */
 var EventEmitter = require('events').EventEmitter;
-var crypto = require('crypto');
+var NodeRSA = require('node-rsa');
 var statusMessages = {
 	success: [
 		'Passed',
@@ -28,27 +28,24 @@ function signRequest(repoSlug, userToken) {
 	return crypto.createHash('sha256').update(repoSlug + userToken).digest('hex');
 }
 
-function create(token) {
+function create(publicKey) {
 
 	var handler = function (req, res, next) {
+		if (!sig) {
+			return hasError('No Signature found on request');
+		}
+		if (!repoSlug) {
+			return hasError('No repo found on request');
+		}
+		var key = new NodeRSA(publicKey, {signingScheme: 'sha1'});
+
 		function hasError(msg) {
 			var err = new Error(msg);
 			next(err);
 		}
 
-		if (token) {
-			var repoSlug = req.headers['travis-repo-slug'];
-			var sig = req.headers['authorization'];
-			if (!sig) {
-				return hasError('No authorization found on request');
-			}
-			if (!repoSlug) {
-				return hasError('No repo found on request');
-			}
-			if (sig !== signRequest(repoSlug, token)) {
-				return hasError('Authentication does not match');
-			}
-		}
+		if (!key.verify(JSON.parse(req.body.payload), sig, 'base64', 'base64'))
+			return hasError('Signed payload does not match signature');
 
 		var result;
 		try {
@@ -57,7 +54,7 @@ function create(token) {
 			return hasError(err.message);
 		}
 		var status = null;
-		Object.keys(statusMessages).some(key=> {
+		Object.keys(statusMessages).some(key => {
 			var value = statusMessages[key];
 			if (value.indexOf(result.status_message) !== -1) {
 				status = key;
@@ -69,7 +66,7 @@ function create(token) {
 		res.json({ok: true});
 
 		handler.emit('*', result);
-		handler.emit(status, result);
+		handler.emit(event, result);
 	};
 	handler.emitter = new EventEmitter();
 	bindEmitter(handler, handler.emitter);
